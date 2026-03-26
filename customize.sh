0649d08bcb1c600013d640fb9cb318baf30772d2
dracediax <60549065+dracediax@users.noreply.github.com>
2026-03-26 13:50:39 +0000

    v1.4: remove BT absolute volume

--- customize.sh
+++ customize.sh
@@ -13,12 +13,6 @@ if [ -f "$CONFIG" ]; then
             if [ "$STREAM" = "both" ]; then
                 echo "ro.config.vc_call_vol_steps=$STEPS" >> "$MODPATH/system.prop"
             fi
-
-            BT_ABSVOL=$(grep '^bt_disable_absvol=' "$SETTINGS" | cut -d= -f2)
-            if [ "$BT_ABSVOL" = "on" ]; then
-                echo "persist.bluetooth.disableabsvol=true" >> "$MODPATH/system.prop"
-                ui_print "- Bluetooth absolute volume disabled"
-            fi
         fi
 
         ui_print "- Volume steps set to $STEPS"
